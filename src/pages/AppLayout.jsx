fa106b19086757880dffaa97a83becb4350507ec
Mohamed Ayman <67555382+MuhAymanZ@users.noreply.github.com>
2023-10-06 04:48:37 +0300

    name fix again

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,13 @@
 // import AppNav from "../components/AppNav";
 import Map from "../components/Map";
-import SideBar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import User from "../components/User";
 import styles from "./AppLayout.module.css";
 function AppLayout() {
 	return (
 		<div className={styles.app}>
 			<User />
-			<SideBar />
+			<Sidebar />
 			<Map />
 		</div>
 	);
